cee023e5ca1c1a76c320d74e22f40d89a0866042
Jose Antonio Fdez-Montes Garcia <jafmgdam@gmail.com>
2023-10-13 11:40:57 +0200

    Actividad terminada

---
+++ app/src/main/java/com/example/articulocompose/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.example.articulocompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.articulocompose.ui.theme.ArticuloComposeTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ArticuloComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    PictureWithText()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Texts() {
+
+
+        Text(
+            text = stringResource(R.string.jetpack_compose_tuto),
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = stringResource(R.string.Short_Description),
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = stringResource(R.string.Big_Description),
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+
+
+
+}
+
+@Composable
+fun PictureWithText(){
+
+    val image = painterResource(R.drawable.bg_compose_background)
+
+    Column {
+        Image(
+            painter = image,
+            contentDescription = null,
+        )
+        Texts()
+    }
+
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ArticlePreview() {
+    ArticuloComposeTheme {
+        PictureWithText()
+    }
+}
